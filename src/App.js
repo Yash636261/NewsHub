0d6eab49f0e9acd507d240b922af77e78107f7e4
yash suthar <ysuthar8055@gmail.com>
2023-01-25 19:24:01 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>hii guys lets use class based components
-      </div>
+      <div>hii guys lets use class based components</div>
     )
   }
 }

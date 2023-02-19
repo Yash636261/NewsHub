9f8fc0f0995250d3b539de87e107b9a9636850f7
yash suthar <ysuthar8055@gmail.com>
2023-02-19 13:53:45 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
     return (
       <div>
         <Navbar/>
-        <NewsSec/>
+        <NewsSec pageSize={6}/>
       </div>
     )
   }
--- src/components/NewsSec.js
+++ src/components/NewsSec.js
@@ -13,7 +13,7 @@ export class NewsSec extends Component {
   }
 
   async componentDidMount(){
-    let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=c5ddccd7e12e45a6bcae2b4617dc6570&page=1&pageSize=20";
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c5ddccd7e12e45a6bcae2b4617dc6570&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
@@ -22,11 +22,8 @@ export class NewsSec extends Component {
 
   handlenext= async () =>{
 
-    if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){
-
-    }
-    else{
-      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c5ddccd7e12e45a6bcae2b4617dc6570&page=${this.state.page + 1}&pageSize=20`;
+    if(! this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+      let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c5ddccd7e12e45a6bcae2b4617dc6570&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json()
       console.log(parsedData);
@@ -35,12 +32,11 @@ export class NewsSec extends Component {
         page: this.state.page + 1,
         articles: parsedData.articles
       })
-
     }
   }
   handleprev= async ()=>{
 
-    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c5ddccd7e12e45a6bcae2b4617dc6570&page=${this.state.page - 1}&pageSize=20`;
+    let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=c5ddccd7e12e45a6bcae2b4617dc6570&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
@@ -55,7 +51,7 @@ export class NewsSec extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>this is the news section</h2>
+        <h1 className="text-center">this is the news section</h1>
         <div className="row">
           {this.state.articles.map((element)=>{
             return <div className="col-md-4"key={element.url} >
@@ -66,7 +62,7 @@ export class NewsSec extends Component {
         </div>
         <div className="container d-flex justify-content-between">
           <button disabled={this.state.page<1} type="button" className="btn btn-dark" onClick={this.handleprev}>&larr; previous</button>
-          <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-dark" onClick={this.handlenext}>next &rarr;</button>
+          <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handlenext}>next &rarr;</button>
         </div>
       </div>
     )
